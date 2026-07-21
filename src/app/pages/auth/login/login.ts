71410abf6b184c288e551ff7aea8ff1129435499
DESKTOP-GURND3D\TIEN <xuantien24689@gmail.com>
2026-07-21 16:44:44 +0700

    fix ui component and auth

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,30 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { LucideEyeOff, LucideMessageSquare, LucideEye } from '@lucide/angular';
+import { LucideMessageSquare } from '@lucide/angular';
 import { RouterLink } from "@angular/router";
 import { UiButton } from "@app/share/component/button/button";
+import { UiInput } from "@app/share/component/input/input";
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, LucideMessageSquare, LucideEyeOff, LucideEye, RouterLink, UiButton],
+  imports: [FormsModule, LucideMessageSquare, RouterLink, UiButton, UiInput],
   templateUrl: './login.html',
 })
 export class Login {
-  isTogglePassword = false;
-  email = signal('xuantien@gmail.com');
-  password = signal('123456');
+  email = signal('');
+  password = signal('');
   rememberMe = signal(false);
-  showPassword = signal(false);
-
-  togglePassword() {
-    this.showPassword.update(value => !value);
-  }
-
-  login() {
-    console.log({
-      email: this.email(),
-      password: this.password(),
-      rememberMe: this.rememberMe(),
-    });
-  }
+  isLoading = signal(false);
 }
--- src/app/share/component/input/input.ts
+++ src/app/share/component/input/input.ts
@@ -1,26 +1,35 @@
-import { Component, input, model, signal } from "@angular/core";
+import { Component, input, model, signal, computed } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { LucideEye, LucideEyeOff } from "@lucide/angular";
+
+let uidCounter = 0;
 
 @Component({
     selector: 'app-ui-input',
-    imports: [FormsModule],
-    templateUrl: './input.html'
+    imports: [FormsModule, LucideEye, LucideEyeOff],
+    templateUrl: './input.html',
+    host: {
+        class: 'block'
+    }
 })
-export class UiInput{
+export class UiInput {
     label = input('');
     placeholder = input('');
     type = input<'text' | 'email' | 'password' | 'tel'>('text');
     required = input(false);
+    disabled = input(false);
+    error = input('');
 
     value = model('');
     protected readonly showPassword = signal(false);
+    protected readonly inputId = `ui-input-${uidCounter++}`;
 
-    get inputType(){
-        if(this.type() !== 'password') return this.type();
+    get inputType() {
+        if (this.type() !== 'password') return this.type();
         return this.showPassword() ? 'text' : 'password';
     }
 
-    togglePassword(){
-        this.showPassword.update(value => !value);
+    togglePassword() {
+        this.showPassword.update(v => !v);
     }
 }
